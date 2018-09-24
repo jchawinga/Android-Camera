efb4dadcf33d115d016a391ff7f6d7b3a0fc2c06
John Chawinga <jchawinga@nitel.mw>
2018-09-24 14:43:26 +0200

    release version

---
+++ Rossana/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Provider;
+using Android.Content;
+using Android.Runtime;
+using Android.Graphics;
+
+namespace Rossana
+{
+    [Activity(Label = "Rossana", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        ImageView imageView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var btnCamera = FindViewById<Button>(Resource.Id.btnCamera);
+            imageView = FindViewById<ImageView>(Resource.Id.imageView);
+
+            btnCamera.Click += BtnCamera_Click;
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap bitmap = (Bitmap)data.Extras.Get("data");
+        }
+
+        private void BtnCamera_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
+        }
+    }
+}
+
